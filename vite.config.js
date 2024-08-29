e2939c525071f544b1b12543ada1e03bce1505f4
meijianming <mjm13@126.com>
2024-08-29 16:44:41 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json' with { type: 'json' } // Node >=17
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    crx({ manifest }),
+  ],
+})
+
